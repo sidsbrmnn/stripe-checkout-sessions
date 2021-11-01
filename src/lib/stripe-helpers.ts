2a57056861572fea81baafd8f5df80bdedbde8f2
Siddharth Subramanian <sidsbrmnn@gmail.com>
2021-11-01 21:44:55 +0530

    Update stripe helpers with Interval

--- src/lib/stripe-helpers.ts
+++ src/lib/stripe-helpers.ts
@@ -1,23 +1,48 @@
 import Stripe from "stripe";
 
-// Create type CleaningIntercval for weekly, biweekly, monthly
-export type CleaningInterval = "weekly" | "biweekly" | "monthly";
+// Create type Interval for weekly, biweekly, monthly
+export type Interval = "Weekly" | "Every 2 weeks" | "Every 3 weeks";
 
-// Get recurring price data from cleaningInterval
+// Get recurring price data from interval
 export const getRecurringPrice = (
-  cleaningInterval: CleaningInterval
-): Stripe.Checkout.SessionCreateParams.LineItem.PriceData.Recurring => {
-  switch (cleaningInterval) {
-    case "weekly":
+  interval: Interval
+): Stripe.SubscriptionCreateParams.Item.PriceData.Recurring => {
+  switch (interval) {
+    case "Weekly":
       return { interval: "week", interval_count: 1 };
-    case "biweekly":
+    case "Every 2 weeks":
       return { interval: "week", interval_count: 2 };
-    case "monthly":
-      return { interval: "week", interval_count: 4 };
+    case "Every 3 weeks":
+      return { interval: "week", interval_count: 3 };
     default:
-      return undefined;
+      throw new Error("Invalid cleaning interval");
   }
 };
+
+export function formatAmountForDisplay(amount: number): string {
+  let numberFormat = new Intl.NumberFormat(["en-US"], {
+    style: "currency",
+    currency: "USD",
+    currencyDisplay: "symbol",
+  });
+  return numberFormat.format(amount);
+}
+
+export function formatAmountForStripe(amount: number): number {
+  let numberFormat = new Intl.NumberFormat(["en-US"], {
+    style: "currency",
+    currency: "USD",
+    currencyDisplay: "symbol",
+  });
+  const parts = numberFormat.formatToParts(amount);
+  let zeroDecimalCurrency: boolean = true;
+  for (let part of parts) {
+    if (part.type === "decimal") {
+      zeroDecimalCurrency = false;
+    }
+  }
+  return zeroDecimalCurrency ? amount : Math.round(amount * 100);
+}
 
 // Export Stripe object
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
